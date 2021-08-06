fb03ed232dcfd5ea37b755288ca5f96a19002b3e
Hemamithan <hemasn111@gmail.com>
2021-08-06 16:56:30 +0530

    for practice_pushActi

---
+++ Actitime1/src/test/java/com/acttime/testscript/CustomerModule.java
@@ -0,0 +1,18 @@
+package com.acttime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	@Test
+	public void testCreateCustomer()
+	{
+		
+	}
+	@Test
+	public void testModifyCustomer()
+	{
+	   Reporter.log("hi");
+	}
+
+}
